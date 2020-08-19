9c6d312577a2fb094245b85d023a18b2a6ba8f86
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-19 20:43:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,21 +1,22 @@
-# Write your code here!
-def game hash {
-  home: {
-    team_name: "Brooklyn Nets",
-    colors:, ["Black", "White"]
-    players: [
-      {player_name: "Alan Anderson",
-      number: 0, 
-      show: 16, 
-      points: 22,
-      rebounds: 12,
-      assists: 12,
-      steals: 3, 
-      blocks: 1, 
-      slam_dunks: 1 
-      },
-      {
-        player_name: "Reggie Evans",
+def game_hash
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        {
+          player_name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        {
+          player_name: "Reggie Evans",
           number: 30,
           shoe: 14,
           points: 12,
@@ -123,4 +124,3 @@ def game hash {
     }
   }
 end
-      
