4ef86dd2f11abcbb126a0be69811d50cf73d74b7
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-19 20:42:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,23 @@
 # Write your code here!
 def game hash {
-  :home{
-    :team name, 
-    :colors, 
-    :players,
+  home: {
+    team_name: "Brooklyn Nets",
+    colors:, ["Black", "White"]
+    players: [
+      {player_name: "Alan Anderson",
+      number: 0, 
+      show: 16, 
+      points: 22,
+      rebounds: 12,
+      assists: 12,
+      steals: 3, 
+      blocks: 1, 
+      slam_dunks: 1 
+      },
+      {
+        
+      }
+      ]
     
   }
 }
